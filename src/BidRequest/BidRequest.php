9820e4b1bb3d2d59f5931074b9d63bc55d32379b
Daniil Petrov <bbkas.ua@gmail.com>
2022-02-05 21:27:32 +0200

    WIP: add BidRequest

--- src/BidRequest/BidRequest.php
+++ src/BidRequest/BidRequest.php
@@ -84,4 +84,327 @@ class BidRequest
     private Source $source;
     private Regs   $regs;
     private Ext    $ext;
+
+    /**
+     * @param string $id
+     * @param int $test
+     * @param int $tmax
+     */
+    public function __construct(string $id, int $tmax, int $at = 2, int $test = 0)
+    {
+        $this->id = $id;
+        $this->test = $test;
+        $this->at = $at;
+        $this->tmax = $tmax;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Imp
+     */
+    public function getImp(): Imp
+    {
+        return $this->imp;
+    }
+
+    /**
+     * @param Imp $imp
+     */
+    public function setImp(Imp $imp): void
+    {
+        $this->imp = $imp;
+    }
+
+    /**
+     * @return Site
+     */
+    public function getSite(): Site
+    {
+        return $this->site;
+    }
+
+    /**
+     * @param Site $site
+     */
+    public function setSite(Site $site): void
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * @return App
+     */
+    public function getApp(): App
+    {
+        return $this->app;
+    }
+
+    /**
+     * @param App $app
+     */
+    public function setApp(App $app): void
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @return Device
+     */
+    public function getDevice(): Device
+    {
+        return $this->device;
+    }
+
+    /**
+     * @param Device $device
+     */
+    public function setDevice(Device $device): void
+    {
+        $this->device = $device;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTest(): int
+    {
+        return $this->test;
+    }
+
+    /**
+     * @param int $test
+     */
+    public function setTest(int $test): void
+    {
+        $this->test = $test;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAt(): int
+    {
+        return $this->at;
+    }
+
+    /**
+     * @param int $at
+     */
+    public function setAt(int $at): void
+    {
+        $this->at = $at;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTmax(): int
+    {
+        return $this->tmax;
+    }
+
+    /**
+     * @param int $tmax
+     */
+    public function setTmax(int $tmax): void
+    {
+        $this->tmax = $tmax;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getWseat(): array
+    {
+        return $this->wseat;
+    }
+
+    /**
+     * @param string[] $wseat
+     */
+    public function setWseat(array $wseat): void
+    {
+        $this->wseat = $wseat;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getBseat(): array
+    {
+        return $this->bseat;
+    }
+
+    /**
+     * @param string[] $bseat
+     */
+    public function setBseat(array $bseat): void
+    {
+        $this->bseat = $bseat;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getCur(): array
+    {
+        return $this->cur;
+    }
+
+    /**
+     * @param string[] $cur
+     */
+    public function setCur(array $cur): void
+    {
+        $this->cur = $cur;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getWlang(): array
+    {
+        return $this->wlang;
+    }
+
+    /**
+     * @param string[] $wlang
+     */
+    public function setWlang(array $wlang): void
+    {
+        $this->wlang = $wlang;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getBcat(): array
+    {
+        return $this->bcat;
+    }
+
+    /**
+     * @param string[] $bcat
+     */
+    public function setBcat(array $bcat): void
+    {
+        $this->bcat = $bcat;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getBadv(): array
+    {
+        return $this->badv;
+    }
+
+    /**
+     * @param string[] $badv
+     */
+    public function setBadv(array $badv): void
+    {
+        $this->badv = $badv;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getBapp(): array
+    {
+        return $this->bapp;
+    }
+
+    /**
+     * @param string[] $bapp
+     */
+    public function setBapp(array $bapp): void
+    {
+        $this->bapp = $bapp;
+    }
+
+    /**
+     * @return Source
+     */
+    public function getSource(): Source
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param Source $source
+     */
+    public function setSource(Source $source): void
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return Regs
+     */
+    public function getRegs(): Regs
+    {
+        return $this->regs;
+    }
+
+    /**
+     * @param Regs $regs
+     */
+    public function setRegs(Regs $regs): void
+    {
+        $this->regs = $regs;
+    }
+
+    /**
+     * @return Ext
+     */
+    public function getExt(): Ext
+    {
+        return $this->ext;
+    }
+
+    /**
+     * @param Ext $ext
+     */
+    public function setExt(Ext $ext): void
+    {
+        $this->ext = $ext;
+    }
+
+    public function isExpired(float $startMicrotime): bool
+    {
+        return round((\microtime(true) - $startMicrotime) / 1000) > $this->tmax;
+    }
 }
---
+++ tests/Tests/BidRequestTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BidRequestTest extends TestCase
+{
+
+    public function testGetAt()
+    {
+    }
+
+    public function testSetWlang()
+    {
+    }
+
+    public function testGetWseat()
+    {
+    }
+
+    public function testSetTest()
+    {
+    }
+
+    public function testGetDevice()
+    {
+    }
+
+    public function testGetCur()
+    {
+    }
+
+    public function testGetSource()
+    {
+    }
+
+    public function testSetBseat()
+    {
+    }
+
+    public function testSetBapp()
+    {
+    }
+
+    public function testSetCur()
+    {
+    }
+
+    public function testGetUser()
+    {
+    }
+
+    public function testSetTmax()
+    {
+    }
+
+    public function testGetRegs()
+    {
+    }
+
+    public function testGetTmax()
+    {
+    }
+
+    public function testGetBadv()
+    {
+    }
+
+    public function testSetRegs()
+    {
+    }
+
+    public function testSetWseat()
+    {
+    }
+
+    public function testSetExt()
+    {
+    }
+
+    public function testSetApp()
+    {
+    }
+
+    public function testGetExt()
+    {
+    }
+
+    public function testSetUser()
+    {
+    }
+
+    public function testGetBcat()
+    {
+    }
+
+    public function testGetBapp()
+    {
+    }
+
+    public function testSetSource()
+    {
+    }
+
+    public function testSetBadv()
+    {
+    }
+
+    public function testSetDevice()
+    {
+    }
+
+    public function testIsExpired()
+    {
+    }
+
+    public function testSetAt()
+    {
+    }
+
+    public function testSetBcat()
+    {
+    }
+
+    public function testGetTest()
+    {
+    }
+
+    public function testGetWlang()
+    {
+    }
+
+    public function testGetBseat()
+    {
+    }
+}
